63ae73c4bdf558cd27d56be72f78773ac69138de
clbokea <clbo@kea.dk>
2021-10-20 09:48:08 +0200

    varchar changed

--- src/main/resources/data/movies.sql
+++ src/main/resources/data/movies.sql
@@ -5,9 +5,9 @@ drop table IF EXISTS movies;
 create table movies (
                         ID int auto_increment primary key,
                         title varchar(100) ,
-                        year int,
-                        length int ,
+                        year varchar(100),
+                        length varchar(100) ,
                         subject varchar(50),
-                        popularity int ,
+                        popularity varchar(100) ,
                         awards varchar(3)
 )
