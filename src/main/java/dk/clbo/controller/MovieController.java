17674355585b9e0002607f6fdcf0745e217d71de
clbokea <clbo@kea.dk>
2021-10-22 23:24:38 +0200

    dat21v1 end

--- src/main/java/dk/clbo/controller/MovieController.java
+++ src/main/java/dk/clbo/controller/MovieController.java
@@ -1,5 +1,7 @@
 package dk.clbo.controller;
 
+import dk.clbo.model.Dummy;
+import dk.clbo.repository.MovieRepository;
 import dk.clbo.services.MovieService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,11 +10,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MovieController {
 
-    MovieService service = new MovieService();
+    private MovieRepository repo;
+    private String name;
+    private Dummy du;
+
+    public MovieController(){
+        repo = new MovieRepository();
+        name = "Claus";
+    }
 
     @GetMapping("/")
     public String index(Model model){
-        model.addAttribute("movies", service.getAllMovies());
+
+        System.out.println(repo.getAllMovies());
+
+        model.addAttribute("movies", repo.getAllMovies());
+        model.addAttribute("xxx", this.name);
+        model.addAttribute("dumm", du.getName());
         return "index";
     }
 }
---
+++ src/main/java/dk/clbo/model/Dummy.java
@@ -0,0 +1,14 @@
+package dk.clbo.model;
+
+public class Dummy {
+
+    private String name = "Claus";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
