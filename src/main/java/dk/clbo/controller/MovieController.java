7f78c4d58329ce02360ef589a2828397e0b34fa9
clbokea <clbo@kea.dk>
2021-10-27 08:35:28 +0200

    before dat21v2

--- src/main/java/dk/clbo/controller/MovieController.java
+++ src/main/java/dk/clbo/controller/MovieController.java
@@ -11,22 +11,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MovieController {
 
     private MovieRepository repo;
-    private String name;
-    private Dummy du;
 
     public MovieController(){
         repo = new MovieRepository();
-        name = "Claus";
     }
 
     @GetMapping("/")
     public String index(Model model){
-
-        System.out.println(repo.getAllMovies());
-
         model.addAttribute("movies", repo.getAllMovies());
-        model.addAttribute("xxx", this.name);
-        model.addAttribute("dumm", du.getName());
         return "index";
     }
 }
