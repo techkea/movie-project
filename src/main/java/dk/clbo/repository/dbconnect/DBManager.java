3ed77e59d0f4a481b812805eac50e92c56414730
clbokea <clbo@kea.dk>
2021-11-12 17:46:10 +0100

    clean up dbmanager

--- src/main/java/dk/clbo/repository/dbconnect/DBManager.java
+++ src/main/java/dk/clbo/repository/dbconnect/DBManager.java
@@ -1,12 +1,8 @@
 package dk.clbo.repository.dbconnect;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
 public class DBManager {
     // fields
