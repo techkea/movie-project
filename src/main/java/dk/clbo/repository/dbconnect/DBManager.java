8f3b9caad9b7a3ea1f4baa0cc67e2c7f02822897
clbokea <clbo@kea.dk>
2021-11-12 17:42:50 +0100

    reformat dbmanager code

--- src/main/java/dk/clbo/repository/dbconnect/DBManager.java
+++ src/main/java/dk/clbo/repository/dbconnect/DBManager.java
@@ -16,23 +16,19 @@ public class DBManager {
     private static Connection connection = null;
 
     // metoder
-    public static Connection getConnection(){
+    public static Connection getConnection() {
 
         if (connection != null) {
-           return connection;
+            return connection;
         }
-        try (InputStream input = new FileInputStream("src/main/resources/application.properties")) {
-            Properties properties = new Properties();
-            properties.load(input);
-            //url = System.getenv("mp_url"); //properties.getProperty("url");
-            url = System.getenv("url");
-            user = System.getenv("mp_user"); //properties.getProperty("user");
-            password = System.getenv("mp_password"); //properties.getProperty("password");
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
+
+        // Dette er ændret fra at læse fra application.properties filen til at læse environment variabler
+        url = System.getenv("url"); //properties.getProperty("url");
+        user = System.getenv("mp_user"); //properties.getProperty("user");
+        password = System.getenv("mp_password"); //properties.getProperty("password");
+
         try {
-            connection = DriverManager.getConnection(url,user, password);
+            connection = DriverManager.getConnection(url, user, password);
         } catch (SQLException e) {
             e.printStackTrace();
         }
